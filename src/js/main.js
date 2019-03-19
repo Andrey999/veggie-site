86f8e29df86e63d63392fcccf65b0187a8fbc639
Andrey999 <1985gromovandrey@gmail.com>
2019-03-19 23:16:55 +0300

    responsive header and main content

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,31 @@
-let buttons = document.querySelectorAll('.tabs__buttons');
+window.onload = function() {
 
-buttons.addEventListener('click', showTabs);
+    let tabs = document.querySelector('.tabs');
 
-function() {
-    
-}
+    tabs.addEventListener('click', showTabs);
+
+    function showTabs(event) {
+        event.preventDefault();
+
+        if (event.target.className === 'tabs__buttons') {
+            //  меняем активную кнопку
+            let buttons = document.getElementsByClassName('tabs__buttons');
+            for(var i = 0; i < buttons.length; i++) {
+                buttons[i].classList.remove('active');
+            }
+            event.target.classList.add('active');
+
+            //   изменяем   контент
+            let dataTab = event.target.getAttribute('data-target'); // берем data-аттрибут
+            let tabContent = document.getElementsByClassName('tabs__content-list');
+            for(let i = 0; i < tabContent.length; i++) {  // перебираем все блоки в tabContent
+               if(dataTab == i) {  // если data аттрибут равен i(0,1,2,3) тогда меняем его стиль
+                   tabContent[i].style.display = 'flex';
+               }
+               else tabContent[i].style.display = 'none';
+            }
+        }
+    }
+
+};
+
