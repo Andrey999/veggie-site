49b5e611e65ef83915e55c8b591a8b0651d41730
Andrey999 <1985gromovandrey@gmail.com>
2019-03-20 22:50:28 +0300

    comit last files

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,25 @@
 window.onload = function() {
+    // burger  menu
+    let menu = document.getElementById('header__menu-burger');
+    let showMenu = document.getElementById('mobile-menu');
+    menu.addEventListener('click', openMenu);
 
+    function openMenu() {
+        let span1 = document.getElementById('line-1');
+        let span2 = document.getElementById('line-2');
+        let span3= document.getElementById('line-3');
+        showMenu.classList.toggle('mobile-menu_show');
+        span1.classList.toggle('line-1');
+        span2.classList.toggle('line-2-hidden');
+        span3.classList.toggle('line-3');
+    }  //////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+
+
+
+
+//tabs
     let tabs = document.querySelector('.tabs');
-
     tabs.addEventListener('click', showTabs);
 
     function showTabs(event) {
